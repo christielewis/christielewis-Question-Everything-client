6926df234fc5fa7fd442083d14b554fc3191065c
Christie Lewis <christielewis@Christies-MacBook-Air.local>
2022-05-25 15:53:18 -0400

    write Update and Delete axios calls

--- src/api/questions.js
+++ src/api/questions.js
@@ -39,3 +39,32 @@ export const showQues = (id, user) => {
     }
   })
 }
+
+export const updateQues = (id, data, user) => {
+  return axios({
+    method: 'PATCH',
+    url: apiUrl + '/questions/' + id,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    },
+    data: {
+      question: {
+        title: data.title,
+        topic: data.topic,
+        description: data.description,
+        season: data.season,
+        episode: data.episode
+      }
+    }
+  })
+}
+
+export const deleteQues = (id, user) => {
+  return axios({
+    method: 'DELETE',
+    url: apiUrl + '/questions/' + id,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
