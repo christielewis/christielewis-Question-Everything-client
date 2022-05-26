4208096f052123cdde449274cc270bd6712d4ba0
Christie Lewis <christielewis@Christies-MacBook-Air.local>
2022-05-26 12:35:25 -0400

    Created axios calls for answers

---
+++ src/api/answers.js
@@ -0,0 +1,42 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+export const createAns = (data, user) => {
+  return axios({
+    method: 'POST',
+    url: apiUrl + '/answers',
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    },
+    data: {
+      answer: {
+        text: data.text
+      }
+    }
+  })
+}
+
+export const updateAns = (id, data, user) => {
+  return axios({
+    method: 'PATCH',
+    url: apiUrl + '/answers/' + id,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    },
+    data: {
+      answer: {
+        text: data.text
+      }
+    }
+  })
+}
+
+export const deleteAns = (id, user) => {
+  return axios({
+    method: 'DELETE',
+    url: apiUrl + '/answers/' + id,
+    headers: {
+      Authorization: `Bearer ${user.token}`
+    }
+  })
+}
